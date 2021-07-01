f2110028e30db4864f94813af64f7bf2a327ede9
Nancy Zaky <nancyzki@yahoo.com>
2021-07-01 13:58:45 -0700

    Assignment complete

--- main.js
+++ main.js
@@ -5,7 +5,40 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+ const handleClick = (e) => {
+  let heartNode = e.target
+  console.log(heartNode.textContent)
+  mimicServerCall()
+ .then(() => {
+   if(heartNode.textContent === "♡") {
+ heartNode.textContent = '♥';
+ heartNode.classList.add('activated-heart')
+   } else {
+     heartNode.textContent= '♡';
+     heartNode.classList.remove('activated-heart')
+   }
+ }
+ )
+.catch(error => {
+  let errorMes = error.message;
+  let errorDiv = document.querySelector('div#modal');
+  errorDiv.classList.remove('hidden')
+  let errorMessage = document.querySelector('div p#modal-message');
+  let nodeText = document.createTextNode(errorMes);
+  errorMessage.appendChild(nodeText);
+  setTimeout(function() {
+    errorDiv.classList.add('hidden')
+  }, 3000)
+})
+}
 
+//heartNode.classList.add('like-glyph-red')
+const heart = document.querySelectorAll('footer ul li span.like-glyph');
+for (let item of heart) {
+ item.addEventListener('click', handleClick )
+}
+
+//id="modal-message"
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
